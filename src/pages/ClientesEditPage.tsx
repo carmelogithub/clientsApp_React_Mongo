fcaf446810b65c00e9cf920898d5b6903e7951bf
Carmelo Escribano <carmelo.escribano@gmail.com>
2026-07-22 11:56:40 +0200

    cambios update

--- src/pages/ClientesEditPage.tsx
+++ src/pages/ClientesEditPage.tsx
@@ -1,29 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ClientService } from "../services/clientService";
+import { useLocation } from "react-router";
+import { Client } from "../models/Client";
+import { IonInput, IonPage, useIonViewDidEnter } from "@ionic/react";
 
-export default function ClientesEditPage(){
-    const [client, setClient] = useState({ _id:"", nombre: "", ciudad: "", facturacion: 0 });
-    const save = async () => {
+export default function ClientesEditPage() {
+  const [client, setClient] = useState({
+    _id: "",
+    nombre: "",
+    ciudad: "",
+    facturacion: 0,
+  });
+const location = useLocation();
+  const cliente:any = location.state;
+//  console.log(cliente);
+  
+const save = async () => {
     await ClientService.updateClient(client);
     //navigate('/clients');
     //history.push("/clients");
   };
+  
+  useEffect(()=>{
+    if(cliente){
+      setClient(cliente);
+    }
+},[]
+);
+ 
 
-    return(
-        <div>
-            <input placeholder="id"
-            onChange={(e) => setClient({ ...client, _id: e.target.value })}
-          />
-            <input placeholder="nombre"
-            onChange={(e) => setClient({ ...client, nombre: e.target.value })}
-          />
-          <input placeholder="ciudad"
-            onChange={(e) => setClient({ ...client, ciudad: e.target.value })}
-          />
-          <input placeholder="facturacion"
-            onChange={(e) => setClient({ ...client, facturacion: Number(e.target.value) })}
-          />
-          <button onClick={save}>Guardar</button>
-        </div>
-    );
+  return (
+    <IonPage>
+    <div>
+      <input
+        value={client._id}
+        placeholder="id"
+        onChange={(e) => setClient({ ...client, _id: e.target.value })}
+      />
+      <IonInput
+      value={client.nombre}
+        placeholder="nombre"
+        onIonInput={(e) => setClient({ ...client, nombre: e.detail.value || "" })}
+      />
+      <input
+      value={client.ciudad}
+        placeholder="ciudad"
+        onChange={(e) => setClient({ ...client, ciudad: e.target.value })}
+      />
+      <input
+      value={client.facturacion}
+        placeholder="facturacion"
+        onChange={(e) =>
+          setClient({ ...client, facturacion: Number(e.target.value) })
+        }
+      />
+      <button onClick={save}>Actualizar</button>
+    </div>
+    </IonPage>
+  );
 }
--- src/pages/ClientesPage.tsx
+++ src/pages/ClientesPage.tsx
@@ -1,44 +1,56 @@
-import { IonContent, IonPage, useIonViewDidEnter } from "@ionic/react";
+import { IonButton, IonContent, IonPage, useIonViewDidEnter } from "@ionic/react";
 import { useState } from "react";
 import { ClientService } from "../services/clientService";
+import { useHistory } from "react-router";
 
-export default function ClientesPage(){
-
-const [clients, setClients] = useState<any[]>([]);
-
-const loadClients = async () => {
+export default function ClientesPage() {
+  const [clients, setClients] = useState<any[]>([]);
+  const history=useHistory();
+  
+  const loadClients = async () => {
     const datos = await ClientService.getClients();
     setClients(datos);
   };
 
+  const formatCurrency = (value:number) => {
+    return new Intl.NumberFormat("es-ES", {
+      style: "currency",
+      currency: "EUR",
+    }).format(value);
+  };
+   
   useIonViewDidEnter(() => {
-    loadClients();  
+    loadClients();
   }, []);
 
-    return(
-        <IonPage>
-             <IonContent>
-                <h2>Tabla de clientes</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre</th>
-                <th>Ciudad</th>
-                <th>Facturación</th>
-              </tr>
-            </thead>
+  return (
+    <IonPage>
+      <IonContent>
+        <h2>Tabla de clientes</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Ciudad</th>
+              <th>Facturación</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
 
-            <tbody>
-              {clients.map((cliente) => (
-                <tr key={cliente._id}>
-                  <td>{cliente.nombre}</td>
-                  <td>{cliente.ciudad}</td>
-                  <td>{cliente.facturacion}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-             </IonContent>
-        </IonPage>
-    );
+          <tbody>
+            {clients.map((cliente) => (
+              <tr key={cliente._id}>
+                <td>{cliente._id}</td>
+                <td>{cliente.nombre}</td>
+                <td>{cliente.ciudad}</td>
+                <td className="text-end">{formatCurrency(cliente.facturacion)}</td>
+                <td><IonButton onClick={()=>{history.push('/editar',cliente)}}>Editar</IonButton></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </IonContent>
+    </IonPage>
+  );
 }
